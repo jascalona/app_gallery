73cf0db9e7485d5f015e5a6e618ebdb786e90f8b
jaescalona <abrahamjose.1587@gmail.com>
2024-07-12 10:14:56 -0400

    carga de controlador para crear directorios

--- DOC/doc.php
+++ DOC/doc.php
@@ -48,27 +48,35 @@
                         Crear Directorio
                     </button>
 
-                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                            <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="exampleModalLabel">Nuevo Directorio</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                
-                                <div class="mb-3">
-                                    <input type="text" class="form-control" id="folder" placeholder="Nombre del Directorio">
+                        <?php
+                            include "./Controller/create_directory.php";    
+                        ?>
+
+                            <!-- method POST -->
+                        <form action="" method="POST">
+
+                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Nuevo Directorio</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
-                                
+                                <div class="modal-body">
+                                    
+                                    <div class="mb-3">
+                                        <input type="text" class="form-control" id="folder" placeholder="Nombre del Directorio">
+                                    </div>
+                                    
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                    <button type="submit" name="create_directory" value="submit" class="btn btn-primary">Crear</button>
+                                </div>
+                                </div>
                             </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                                <button type="submit" class="btn btn-primary">Crear</button>
                             </div>
-                            </div>
-                        </div>
-                        </div>
+                            </form>
 
                     <li><a href=""><i class='bx bx-list-ul'></i></a></li>
                     <li><a href=""><i class='bx bxs-grid'></i></a></li>
