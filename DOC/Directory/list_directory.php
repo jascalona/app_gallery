07620a2fecd553485d4a8e577693ca65c13843cd
jaescalona <abrahamjose.1587@gmail.com>
2024-07-16 16:18:43 -0400

    new

--- DOC/Directory/list_directory.php
+++ DOC/Directory/list_directory.php
@@ -5,7 +5,7 @@ $directorio = opendir("Directory");
 while ($elemento = readdir($directorio)) {
     if (is_dir("./Directory/" . $elemento)) {
 
-    $listar .= "<li><i class='bx bxs-folder-open' style='color:#eac11a'></i><a href='./Directory/$elemento' target='_blank'>$elemento</a></li>";
+    $listar .= "<li><i class='bx bxs-folder-open' style='color:#DDCE78'></i><a href='./Directory/$elemento' target='_blank'>$elemento</a></li>";
 
     }
 
