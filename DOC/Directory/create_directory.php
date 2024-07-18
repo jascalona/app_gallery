8f08ba143800b0c75be1aebd3b1ab75972f88b5a
jaescalona <abrahamjose.1587@gmail.com>
2024-07-18 16:48:43 -0400

    validar is_dir para selecionar directorio

--- DOC/Directory/create_directory.php
+++ DOC/Directory/create_directory.php
@@ -4,13 +4,13 @@ if (!empty($_POST['btn-c'])) {
 
     // Variable Name_directory
     $directory = $_POST['name_folder'];
-
+    // validar ma;ana $rute(is_dir('./Directory'));
     
     if(!mkdir($directory, 0777, true)) {
         die('<script>alert("Ha Ocurrido un Error!")</script>');
 
     } else {
-        echo ('<script>alert("Directorio creado con Exito!")</script>');
+     //   echo ('<script>alert("Directorio creado con Exito!")</script>');
         header('location: http://localhost/app_gallery/DOC/doc.php');
     }
     
