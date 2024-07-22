b7e8d57f13c2bca125c38c33a053ba67697e562c
jaescalona <abrahamjose.1587@gmail.com>
2024-07-22 12:38:13 -0400

    new

--- DOC/Directory_form/list_directory_form.php
+++ DOC/Directory_form/list_directory_form.php
@@ -2,13 +2,13 @@
 
 // List directory 
 $list = null;
-$rute_directory = opendir("Directory_form");
+$rute_directory = opendir("./Directory_form/");
 while ($elemento = readdir($rute_directory)) {
 
     if($elemento != '.' && $elemento != '..')
     if (is_dir("./Directory_form/" . $elemento)) {
 
-        $listar .= "<li><i class='bx bxs-folder-open' style='color:#DDCE78'></i><a href='./Directory/$elemento' target='_blank'>$elemento</a>
+        $listar .= "<li><i class='bx bxs-folder-open' style='color:#DDCE78'></i><a href='./Directory_form/$elemento' target='_blank'>$elemento</a>
           
                         <button  style='border: none; position: absolute; margin-left: 60%;' type='button' class='btn' data-bs-toggle='dropdown'>
                             <i class='bx bx-dots-horizontal-rounded'></i>
