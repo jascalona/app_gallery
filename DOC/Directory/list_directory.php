38a81dcf0af6f8102ecb63eab4687d3c5b89f930
jaescalona <abrahamjose.1587@gmail.com>
2024-07-22 10:55:13 -0400

    new

--- DOC/Directory/list_directory.php
+++ DOC/Directory/list_directory.php
@@ -9,7 +9,7 @@ while ($elemento = readdir($directorio)) {
 
     $listar .= "<li><i class='bx bxs-folder-open' style='color:#DDCE78'></i><a href='./Directory/$elemento' target='_blank'>$elemento</a>
           
-                    <button  style='border: none; position: absolute;' type='button' class='btn' data-bs-toggle='dropdown'>
+                    <button  style='border: none; position: absolute; margin-left: 60%;' type='button' class='btn' data-bs-toggle='dropdown'>
                         <i class='bx bx-dots-horizontal-rounded'></i>
                     </button>   
                         <ul style='margin-right: ; width: auto;' class='dropdown-menu dropdown-menu-dark'>
@@ -19,10 +19,7 @@ while ($elemento = readdir($directorio)) {
                             <li><button class='dropdown-item' type='button'><i class='bx bx-trash'></i>Mover a la papelera</button></li>
                         </ul>
         
-
-        
-        
-    </li>";
+                </li>";
 
     }
 
--- DOC/Directory_form/list_directory_form.php
+++ DOC/Directory_form/list_directory_form.php
@@ -8,8 +8,19 @@ while ($elemento = readdir($rute_directory)) {
     if($elemento != '.' && $elemento != '..')
     if (is_dir("./Directory_form/" . $elemento)) {
 
-    $listar .= "<li><i class='bx bxs-folder-open' style='color:#DDCE78'></i><a href='./Directory/$elemento' target='_blank'>$elemento</a></li>";
+        $listar .= "<li><i class='bx bxs-folder-open' style='color:#DDCE78'></i><a href='./Directory/$elemento' target='_blank'>$elemento</a>
+          
+                        <button  style='border: none; position: absolute; margin-left: 60%;' type='button' class='btn' data-bs-toggle='dropdown'>
+                            <i class='bx bx-dots-horizontal-rounded'></i>
+                        </button>   
+                            <ul style='margin-right: ; width: auto;' class='dropdown-menu dropdown-menu-dark'>
+                                <li><button class='dropdown-item' type='button'><i class='bx bx-folder-open'></i>Abrir</button></li>
+                                <li><button class='dropdown-item' type='button'><i class='bx bx-code-alt'></i>Gestional enlace</button></li>
+                                <li><button class='dropdown-item' type='button'><i class='bx bxs-star-half' ></i>Agregar a Favoritos</button></li>
+                                <li><button class='dropdown-item' type='button'><i class='bx bx-trash'></i>Mover a la papelera</button></li>
+                            </ul>
 
+                    </li>";
     }
 
 }
