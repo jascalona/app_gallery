b188a5f95fc793c066c98480631c351f10edd3cf
jaescalona <abrahamjose.1587@gmail.com>
2024-07-22 14:35:09 -0400

    new

--- DOC/Directory_form/create_directory_form.php
+++ DOC/Directory_form/create_directory_form.php
@@ -5,15 +5,19 @@ if (!empty($_POST['btn-d'])) {
     // Variable Name_directory
     $directory_form = $_POST['name_form'];
 
-    
+    if (!file_exists($directory_form)) {
+
     if(!mkdir($directory_form, 0777, true)) {
         die('<script>alert("Ha Ocurrido un Error!")</script>');
 
     } else {
-        echo ('<script>alert("Directorio creado con Exito!")</script>');
         header('location: http://localhost/app_gallery/DOC/form_install.php');
     }
     
+
+} else{
+    die ('<script>alert("Ya hay un elemento con el mismo nombre!")</script>');
+}
 
 
 }
