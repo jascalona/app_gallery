e1b1f64e24ec8040de3ca1e7de4e8ac2538d7fca
jaescalona <abrahamjose.1587@gmail.com>
2024-07-22 10:37:21 -0400

    ajuste modal

--- DOC/Directory/list_directory.php
+++ DOC/Directory/list_directory.php
@@ -9,13 +9,14 @@ while ($elemento = readdir($directorio)) {
 
     $listar .= "<li><i class='bx bxs-folder-open' style='color:#DDCE78'></i><a href='./Directory/$elemento' target='_blank'>$elemento</a>
           
-                    <button  style='border: none; position: absolute; padding-right: %;' type='button' class='btn' data-bs-toggle='dropdown'>
+                    <button  style='border: none; position: absolute;' type='button' class='btn' data-bs-toggle='dropdown'>
                         <i class='bx bx-dots-horizontal-rounded'></i>
                     </button>   
-                        <ul class='dropdown-menu dropdown-menu-end'>
-                            <li><button class='dropdown-item' type='button'>Action</button></li>
-                            <li><button class='dropdown-item' type='button'>Another action</button></li>
-                            <li><button class='dropdown-item' type='button'>Something else here</button></li>
+                        <ul style='margin-right: ; width: auto;' class='dropdown-menu dropdown-menu-dark'>
+                            <li><button class='dropdown-item' type='button'><i class='bx bx-folder-open'></i>Abrir</button></li>
+                            <li><button class='dropdown-item' type='button'><i class='bx bx-code-alt'></i>Gestional enlace</button></li>
+                            <li><button class='dropdown-item' type='button'><i class='bx bxs-star-half' ></i>Agregar a Favoritos</button></li>
+                            <li><button class='dropdown-item' type='button'><i class='bx bx-trash'></i>Mover a la papelera</button></li>
                         </ul>
         
 
